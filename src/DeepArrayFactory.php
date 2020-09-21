a0b3573e3a88c0d1382e035f7be5cf174d55446c
Julian <jsawicki@internetcompany.nl>
2020-09-21 11:15:15 +0200

    Code review

--- src/DeepArrayFactory.php
+++ src/DeepArrayFactory.php
@@ -19,7 +19,15 @@ class DeepArrayFactory
         $array = [];
         $index = array_shift($indices);
         if (null === $index) {
-            throw new InvalidArgumentException('Given indices can not be empty');
+			// This state probably wont happen, because of this reasons:
+			// 1) Fill would fail if this var was supplied: $indices = [0,1,2,null];
+			// Because of the type declaration: int ... 
+			// php will throw fatal errors if [0,1,2,null] would be supplied to fill(). 
+			// The parse method of IntegerParser type casts to int. A null would become int 0.
+			// 
+			// 2) array_shift return NULL when an empty array is supplied. 
+			// The fill method stops calling itself when the indices array is empty. 
+			throw new InvalidArgumentException('Given indices can not be empty');
         }
         if (false === empty($indices)) {
             $array[$index] = $this->fill($value, ...$indices);
